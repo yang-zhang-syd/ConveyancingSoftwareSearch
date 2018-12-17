3ec5e2339c4074b9b01ad9d36c8e547b40018eb0
Yang Zhang <yang.zhang@icloud.com>
2018-12-17 19:39:10 +1100

    Switched to use regular expression.

--- ConveyancingSoftwareSearch/Program.cs
+++ ConveyancingSoftwareSearch/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace ConveyancingSoftwareSearch
@@ -17,10 +21,45 @@ namespace ConveyancingSoftwareSearch
             var request = WebRequest.CreateHttp(uriString);
             var responseStream = request.GetResponse().GetResponseStream();
 
-            var parser = new SimpleHtmlParser();
-            var tag = parser.ParseHtml(responseStream);
+            var sr = new StreamReader(responseStream);
+            var response = sr.ReadToEnd();
 
-            Console.Read();
+            var regex = new Regex("<div class=\"g\">");
+            var matches = regex.Matches(response);
+            var resultsBeginIndexes = matches
+                .Select(m => m.Index)
+                .OrderBy(i => i)
+                .ToList();
+
+            //foreach (var idx in resultsBeginIndexes)
+            //{
+            //    Console.WriteLine(response.Substring());
+            //}
+
+            regex = new Regex(@"www\.smokeball\.com\.au");
+            matches = regex.Matches(response);
+            var smokeballBeginIndexes = matches
+                .Select(m => m.Index)
+                .OrderBy(i => i)
+                .ToList();
+
+
+            foreach (var idx in smokeballBeginIndexes)
+            {
+                Console.WriteLine(response.Substring(idx, 100));
+            }
+
+            var resultsIndexes = new List<int>();
+            foreach (var sidx in smokeballBeginIndexes)
+            {
+                var resultBeginAt = resultsBeginIndexes.First(i => i > sidx);
+                var resultIndex = resultsBeginIndexes.IndexOf(resultBeginAt);
+                resultsIndexes.Add(resultIndex);
+            }
+
+            resultsIndexes = resultsIndexes.Distinct().OrderBy(i => i).ToList();
+
+            Console.WriteLine(string.Join(',', resultsIndexes));
         }
     }
 }
