549877221e527539bddae01305a9f0a3397f8122
Yang Zhang <yang.zhang@icloud.com>
2018-12-18 21:38:32 +1100

    clean up

--- ConveyancingSoftwareSearch/Program.cs
+++ ConveyancingSoftwareSearch/Program.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Web;
 
 namespace ConveyancingSoftwareSearch
 {
